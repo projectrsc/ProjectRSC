b046d55f80594505fd851f52d0425f0a5ba27330
openfrog <openfrog@openfroglabs-pc.(none)>
2012-11-26 18:59:11 -0800

    Trade log bug fix

--- ProjectRSC/Server/src/org/darkquest/gs/db/query/TradeLog.java
+++ ProjectRSC/Server/src/org/darkquest/gs/db/query/TradeLog.java
@@ -1,6 +1,7 @@
 package org.darkquest.gs.db.query;
 
 import java.sql.Connection;
+
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
@@ -8,18 +9,39 @@ import java.util.List;
 import org.darkquest.config.Constants;
 import org.darkquest.gs.model.InvItem;
 
+/**
+ * TODO: Change log to builder design (ex: new TradeLog().build())
+ * Logic should never be involved in preparing statements other than passing data to the database
+ * @author openfrog
+ *
+ */
+
+
 public final class TradeLog extends DatabaseLog {
 
-	private final String player1, player2;
-
-	private final List<InvItem> player1Offer, player2Offer;
+	private String player1, player2, playerOnesOffer, playerTwosOffer;
 
 	public TradeLog(String player1, String player2, List<InvItem> player1Offer, List<InvItem> player2Offer) {
 		super("INSERT INTO `" + Constants.GameServer.MYSQL_TABLE_PREFIX + "trade_logs`(`player1`, `player2`, `player1_items`, `player2_items`, `time`) VALUES(?, ?, ?, ?, ?)");
 		this.player1 = player1;
 		this.player2 = player2;
-		this.player1Offer = player1Offer;
-		this.player2Offer = player2Offer;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (InvItem i : player1Offer) {
+			//System.out.println("Player 1 offered: " + i.getID());
+			sb.append(i.getID()).append(":").append(i.getAmount()).append(",");
+		}
+		
+		playerOnesOffer = sb.toString();
+		sb = new StringBuilder();
+		
+		for (InvItem i : player2Offer) {
+			//System.out.println("Player 2 offered: " + i.getID());
+			sb.append(i.getID()).append(":").append(i.getAmount()).append(",");
+		}
+		
+		playerTwosOffer = sb.toString();
 	}
 
 	@Override
@@ -27,20 +49,8 @@ public final class TradeLog extends DatabaseLog {
 		PreparedStatement statement = connection.prepareStatement(query);
 		statement.setString(1, player1);
 		statement.setString(2, player2);
-
-		StringBuilder sb = new StringBuilder();
-
-		for (InvItem i : player1Offer) {
-			sb.append(i.getID()).append(":").append(i.getAmount()).append(",");
-		}
-		statement.setString(3, sb.toString());
-
-		sb = new StringBuilder();
-
-		for (InvItem i : player2Offer) {
-			sb.append(i.getID()).append(":").append(i.getAmount()).append(",");
-		}
-		statement.setString(4, sb.toString());
+		statement.setString(3, playerOnesOffer);
+		statement.setString(4, playerTwosOffer);
 		statement.setLong(5, time);
 		return statement;
 	}
